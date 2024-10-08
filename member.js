5c40e394ab893c14945a4dd0ff8cb43d7bfe6f01
The White Axe <32323494+rguaita@users.noreply.github.com>
2024-10-08 07:02:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: 'Gerald',
+        age: 20,
+        skills: ['JavaScript', 'React', 'Node', 'Python']
+    }
+    return member.skills;
+}
